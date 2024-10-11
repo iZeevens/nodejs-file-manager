6f812c261d9d63bde09f313fd34504d735480e5f
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-11 19:04:02 +0500

    feat: add start.js

--- src/start.js
+++ src/start.js
@@ -1,2 +1,15 @@
 const args = process.argv.slice(2);
 
+let username = null
+
+args.forEach(element => {
+  if (element.startsWith('--username')) {
+    username = element.split('=')[1]
+  }
+});
+
+if (username) {
+  console.log(username)
+} else {
+  throw new Error('No username provided')
+}
