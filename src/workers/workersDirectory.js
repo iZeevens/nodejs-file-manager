013c31a01e6a3452616fa727ba070b68eb80c226
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-14 18:03:57 +0500

    refactor: workersDirectory

--- src/workers/workersDirectory.js
+++ src/workers/workersDirectory.js
@@ -1,40 +1,56 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 
-//Opertations to owns function
+function upDir(currDir) {
+  const pathUp = path.join(currDir, "..");
+  return pathUp;
+}
+
+async function cdDir(currDir, pathToFile) {
+  try {
+    const pathCd = path.join(currDir, pathToFile);
+    await fs.access(pathCd);
+    return pathCd;
+  } catch {
+    console.error("Operation failed");
+  }
+}
+
+async function lsDir(currDir) {
+  const infoLs = await fs.readdir(currDir);
+  const result = await infoLs.reduce(async (accumP, file) => {
+    const accum = await accumP;
+    const pathToFile = path.join(currDir, file);
+    const stats = await fs.stat(pathToFile);
+
+    if (stats.isFile()) {
+      accum.push([file, "file"]);
+    } else {
+      accum.push([file, "directory"]);
+    }
+
+    return accum;
+  }, Promise.resolve([]));
+  result.sort((a, b) => a[0].localeCompare(b[0]) && a[1].localeCompare(b[1]));
+
+  console.table(result);
+}
+
 async function workerDirectory(opertaions, currDir) {
   const opertaionName = opertaions[0];
   const pathToFile = opertaions[1] || null;
 
-  if (opertaionName === "up") {
-    const pathUp = path.join(currDir, "..");
-    return pathUp;
-  } else if (opertaionName === "cd" && pathToFile) {
-    try {
-      const pathCd = path.join(currDir, pathToFile);
-      await fs.access(pathCd);
-      return pathCd;
-    } catch {
-      console.error("Operation failed");
-    }
-  } else if (opertaionName === "ls") {
-    const infoLs = await fs.readdir(currDir);
-    const result = await infoLs.reduce(async (accumP, file) => {
-      const accum = await accumP;
-      const pathToFile = path.join(currDir, file);
-      const stats = await fs.stat(pathToFile);
+  const operationMap = {
+    up: upDir,
+    cd: cdDir,
+    ls: lsDir,
+  };
 
-      if (stats.isFile()) {
-        accum.push([file, "file"]);
-      } else {
-        accum.push([file, "directory"]);
-      }
+  const operationFunction = operationMap[opertaionName];
 
-      return accum;
-    }, Promise.resolve([]));
-    result.sort((a, b) => a[0].localeCompare(b[0]) && a[1].localeCompare(b[1]));
-
-    console.table(result);
+  if (operationFunction) {
+    const result = await operationFunction(currDir, pathToFile);
+    return result;
   } else {
     console.error("Invalid input");
   }
